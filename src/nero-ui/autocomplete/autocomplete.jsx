e9f148140e82417f666ab4e3fd59c658396a7a98
alpertunga-bile <holy_celestine@hotmail.com>
2025-08-24 18:28:17 +0300

    autocomplete component is added

---
+++ src/nero-ui/autocomplete/autocomplete.jsx
@@ -0,0 +1,104 @@
+import { useEffect, useState } from "react";
+import Trie from "./trie.ts";
+
+function AutoCompleteItem({ name, key = "autocomplete", onClick, onKeyDown }) {
+  return (
+    <li
+      className="autocomplete-item"
+      onClick={onClick}
+      onKeyDown={onKeyDown}
+      tabIndex={0}
+      key={`tag_${key}_${name}`}
+    >
+      {name}
+    </li>
+  );
+}
+
+export default function AutoComplete({ form_name, options }) {
+  const [userInput, setUserInput] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [trie, setTrie] = useState(new Trie());
+
+  useEffect(() => {
+    trie.multi_insert(options);
+    setTrie(trie);
+  }, [options]);
+
+  const handle_input_change = (e) => {
+    const input_value = e.currentTarget.value;
+
+    setUserInput(input_value);
+
+    if (input_value === "") {
+      setSuggestions([]);
+      return;
+    }
+
+    setSuggestions([...trie.starts_with(input_value)]);
+  };
+
+  const handle_key_press = (e) => {
+    if (e.key === "Enter" && userInput.trim() !== "") {
+      e.preventDefault();
+
+      if (suggestions.includes(userInput)) {
+        setSuggestions([]);
+        setUserInput(userInput);
+      }
+
+      if (suggestions.length == 1) {
+        setUserInput(suggestions[0]);
+        setSuggestions([]);
+      }
+    }
+  };
+
+  const suggestion_onclick = (e) => {
+    setSuggestions([]);
+
+    setUserInput(e.target.innerText);
+  };
+
+  const suggestion_onkeydown = (e) => {
+    if (e.key === "Enter" && userInput.trim() !== "") {
+      e.preventDefault();
+
+      if (suggestions.includes(userInput)) {
+        setSuggestions([]);
+        setUserInput(userInput);
+      }
+
+      if (suggestions.length == 1) {
+        setUserInput(suggestions[0]);
+        setSuggestions([]);
+      }
+    }
+  };
+
+  return (
+    <div className="autocomplete-root" ref={root}>
+      <input
+        className="autocomplete-trigger"
+        type="text"
+        placeholder={`Add ${form_name} tags`}
+        onInput={handle_input_change}
+        onKeyDown={handle_key_press}
+        value={userInput}
+      />
+      <ul
+        className="autocomplete-items"
+        key={`${form_name}_searchbar_suggestion`}
+      >
+        {suggestions.map((suggestion, index) => (
+          <AutoCompleteItem
+            name={suggestion}
+            key={`${form_name}_${index}`}
+            onClick={suggestion_onclick}
+            onKeyDown={suggestion_onkeydown}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
