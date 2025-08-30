b5389d31caf4bca5d3d0c78b3400c30262754c67
alpertunga-bile <holy_celestine@hotmail.com>
2025-08-30 17:18:28 +0300

    JSX foreach map is used

--- src/testbed.tsx
+++ src/testbed.tsx
@@ -4,9 +4,19 @@ import Accordion from "./unga-ui/accordion/accordion.jsx";
 import { Dropdown } from "./unga-ui/dropdown/dropdown.jsx";
 import AutoComplete from "./unga-ui/autocomplete/autocomplete.jsx";
 import Chip from "./unga-ui/chip/chip.jsx";
+import { JSX } from "react-jsx";
 
 function App() {
   const auto_options = ["New York", "London", "Honk Kong"];
+
+  const components: JSX.element[] = [
+    (<Accordion>
+      <p>Hello World</p>
+    </Accordion>),
+    (<Dropdown options={["Option 1", "Option 2"]} />),
+    (<AutoComplete form_name="country" options={auto_options} />),
+    (<Chip name="Chip" onClick={() => alert("Hello There")} />),
+  ];
 
   return (
     <>
@@ -21,20 +31,9 @@ function App() {
       </div>
       <h1>Unga-UI is built with Deno + Vite + React</h1>
       <div className="testbed">
-        <div className="testbed-item">
-          <Accordion>
-            <p>Hello World</p>
-          </Accordion>
-        </div>
-        <div className="testbed-item">
-          <Dropdown options={["Option 1", "Option 2"]} />
-        </div>
-        <div className="testbed-item">
-          <AutoComplete form_name="country" options={auto_options} />
-        </div>
-        <div className="testbed-item">
-          <Chip name="Chip" onClick={() => alert("Hello There")} />
-        </div>
+        {components.map((el, index) => (
+          <div key={`testbed_item_${index}`} className="testbed-item">{el}</div>
+        ))}
       </div>
     </>
   );
